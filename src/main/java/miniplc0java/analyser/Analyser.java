499b597ede12abcfdf61416d5677b0f49bff891e
hhidk <295809364@qq.com>
2020-12-08 22:55:22 +0800

    Analyser v1.1

---
+++ src/main/java/miniplc0java/analyser/Analyser.java
@@ -0,0 +1,396 @@
+package miniplc0java.analyser;
+
+import miniplc0java.error.AnalyzeError;
+import miniplc0java.error.CompileError;
+import miniplc0java.error.ErrorCode;
+import miniplc0java.error.ExpectedTokenError;
+import miniplc0java.error.TokenizeError;
+import miniplc0java.instruction.Instruction;
+import miniplc0java.instruction.Operation;
+import miniplc0java.tokenizer.Token;
+import miniplc0java.tokenizer.TokenType;
+import miniplc0java.tokenizer.Tokenizer;
+import miniplc0java.util.Pos;
+
+import java.util.*;
+
+public final class Analyser {
+
+    Tokenizer tokenizer;
+    ArrayList<Instruction> instructions;
+
+    /**
+     * 当前偷看的 token
+     */
+    Token peekedToken = null;
+
+    /**
+     * 符号表
+     */
+    HashMap<String, SymbolEntry> symbolTable = new HashMap<>();
+
+    /**
+     * 下一个变量的栈偏移
+     */
+    int nextOffset = 0;
+
+    public Analyser(Tokenizer tokenizer) {
+        this.tokenizer = tokenizer;
+        this.instructions = new ArrayList<>();
+    }
+
+    public List<Instruction> analyse() throws CompileError {
+        analyseProgram();
+        return instructions;
+    }
+
+    /**
+     * 查看下一个 Token
+     *
+     * @return
+     * @throws TokenizeError
+     */
+    private Token peek() throws TokenizeError {
+        if (peekedToken == null) {
+            peekedToken = tokenizer.nextToken();
+        }
+        return peekedToken;
+    }
+
+    /**
+     * 获取下一个 Token
+     *
+     * @return
+     * @throws TokenizeError
+     */
+    private Token next() throws TokenizeError {
+        if (peekedToken != null) {
+            var token = peekedToken;
+            peekedToken = null;
+            return token;
+        } else {
+            return tokenizer.nextToken();
+        }
+    }
+
+    /**
+     * 如果下一个 token 的类型是 tt，则返回 true
+     *
+     * @param tt
+     * @return
+     * @throws TokenizeError
+     */
+    private boolean check(TokenType tt) throws TokenizeError {
+        var token = peek();
+        return token.getTokenType() == tt;
+    }
+
+    /**
+     * 如果下一个 token 的类型是 tt，则前进一个 token 并返回这个 token
+     *
+     * @param tt 类型
+     * @return 如果匹配则返回这个 token，否则返回 null
+     * @throws TokenizeError
+     */
+    private Token nextIf(TokenType tt) throws TokenizeError {
+        var token = peek();
+        if (token.getTokenType() == tt) {
+            return next();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 如果下一个 token 的类型是 tt，则前进一个 token 并返回，否则抛出异常
+     *
+     * @param tt 类型
+     * @return 这个 token
+     * @throws CompileError 如果类型不匹配
+     */
+    private Token expect(TokenType tt) throws CompileError {
+        var token = peek();
+        if (token.getTokenType() == tt) {
+            return next();
+        } else {
+            throw new ExpectedTokenError(tt, token);
+        }
+    }
+
+    /**
+     * 获取下一个变量的栈偏移
+     *
+     * @return
+     */
+    private int getNextVariableOffset() {
+        return this.nextOffset++;
+    }
+
+    /**
+     * 添加一个符号
+     *
+     * @param name          名字
+     * @param isInitialized 是否已赋值
+     * @param isConstant    是否是常量
+     * @param curPos        当前 token 的位置（报错用）
+     * @throws AnalyzeError 如果重复定义了则抛异常
+     */
+    private void addSymbol(String name, boolean isInitialized, boolean isConstant, Pos curPos) throws AnalyzeError {
+        if (this.symbolTable.get(name) != null) {
+            throw new AnalyzeError(ErrorCode.DuplicateDeclaration, curPos);
+        } else {
+            this.symbolTable.put(name, new SymbolEntry(isConstant, isInitialized, getNextVariableOffset()));
+        }
+    }
+
+    /**
+     * 设置符号为已赋值
+     *
+     * @param name   符号名称
+     * @param curPos 当前位置（报错用）
+     * @throws AnalyzeError 如果未定义则抛异常
+     */
+    private void initializeSymbol(String name, Pos curPos) throws AnalyzeError {
+        var entry = this.symbolTable.get(name);
+        if (entry == null) {
+            throw new AnalyzeError(ErrorCode.NotDeclared, curPos);
+        } else {
+            entry.setInitialized(true);
+        }
+    }
+
+    /**
+     * 获取变量在栈上的偏移
+     *
+     * @param name   符号名
+     * @param curPos 当前位置（报错用）
+     * @return 栈偏移
+     * @throws AnalyzeError
+     */
+    private int getOffset(String name, Pos curPos) throws AnalyzeError {
+        var entry = this.symbolTable.get(name);
+        if (entry == null) {
+            throw new AnalyzeError(ErrorCode.NotDeclared, curPos);
+        } else {
+            return entry.getStackOffset();
+        }
+    }
+
+    /**
+     * 获取变量是否是常量
+     *
+     * @param name   符号名
+     * @param curPos 当前位置（报错用）
+     * @return 是否为常量
+     * @throws AnalyzeError
+     */
+    private boolean isConstant(String name, Pos curPos) throws AnalyzeError {
+        var entry = this.symbolTable.get(name);
+        if (entry == null) {
+            throw new AnalyzeError(ErrorCode.NotDeclared, curPos);
+        } else {
+            return entry.isConstant();
+        }
+    }
+
+    private void analyseProgram() throws CompileError {
+        // program -> item *
+        // item -> function | decl_stmt
+        // decl_stmt -> let_decl_stmt | const_decl_stmt
+
+        while(true) {
+            var peeked = peek();
+            if (peeked.getTokenType() == TokenType.FN_KW) {
+                analyseFunction();
+            }
+            else if (peeked.getTokenType() == TokenType.LET_KW) {
+                analyseLetDeclStmt();
+            }
+            else if(peeked.getTokenType() == TokenType.CONST_KW) {
+                analyseConstDeclStmt();
+            }
+            else {
+                expect(TokenType.EOF);
+                break;
+            }
+        }
+    }
+
+    private void analyseFunction() throws CompileError {
+        // function -> 'fn' IDENT '(' function_param_list? ')' '->' ty block_stmt
+
+        expect(TokenType.FN_KW);
+        expect(TokenType.IDENT);
+        expect(TokenType.L_PAREN);
+        if (peek().getTokenType() == TokenType.CONST_KW ||
+            peek().getTokenType() == TokenType.IDENT) {
+            analyseFunctionParamList();
+        }
+        expect(TokenType.R_PAREN);
+        expect(TokenType.ARROW);
+        String type = analyseType();//todo
+        analyseBlockStmt();
+    }
+
+    private void analyseFunctionParamList() throws CompileError {
+        // function_param_list -> function_param (',' function_param)*
+
+        analyseFunctionParam();
+        while (nextIf(TokenType.COMMA) != null) {
+            analyseFunctionParam();
+        }
+    }
+
+    private void analyseFunctionParam() throws CompileError {
+        // function_param -> 'const'? IDENT ':' ty
+
+        if (peek().getTokenType() == TokenType.CONST_KW) {
+            //todo:const not implemented
+            next();
+        }
+        expect(TokenType.IDENT);//todo: param name not implemented
+        expect(TokenType.COLON);
+        String type = analyseType();//todo
+    }
+
+    private void analyseBlockStmt() throws CompileError {
+        // block_stmt -> '{' stmt* '}'
+        // stmt -> expr_stmt | decl_stmt | if_stmt | while_stmt |
+        //          return_stmt | block_stmt | empty_stmt
+
+        expect(TokenType.L_BRACE);
+        TokenType tokenType = peek().getTokenType();
+        if (tokenType == TokenType.LET_KW) {
+            analyseLetDeclStmt();
+        } else if (tokenType == TokenType.CONST_KW) {
+            analyseConstDeclStmt();
+        } else if (tokenType == TokenType.IF_KW) {
+            analyseIfStmt();
+        } else if (tokenType == TokenType.WHILE_KW) {
+            analyseWhileStmt();
+        } else if (tokenType == TokenType.RETURN_KW) {
+            analyseReturnStmt();
+        } else if (tokenType == TokenType.L_BRACE) {
+            analyseBlockStmt();
+        } else if (tokenType == TokenType.SEMICOLON) {
+            next();
+        } else if (isExpr()){
+            analyseExpr();
+            expect(TokenType.SEMICOLON);
+        } else {
+            throw new Error("Not a statement");
+        }
+        expect(TokenType.R_BRACE);
+    }
+
+    private void analyseLetDeclStmt() throws CompileError {
+        // let_decl_stmt -> 'let' IDENT ':' ty ('=' expr)? ';'
+
+        expect(TokenType.LET_KW);
+        expect(TokenType.IDENT);//todo
+        expect(TokenType.COLON);
+        String type = analyseType();//todo
+        if (peek().getTokenType() == TokenType.ASSIGN) {
+            expect(TokenType.ASSIGN);
+            if (isExpr())
+                analyseExpr();
+            else
+                throw new Error("Expression illegal");
+        }
+        expect(TokenType.SEMICOLON);
+    }
+
+    private void analyseConstDeclStmt() throws CompileError {
+        // const_decl_stmt -> 'const' IDENT ':' ty '=' expr ';'
+
+        expect(TokenType.CONST_KW);
+        expect(TokenType.IDENT);//todo
+        expect(TokenType.COLON);
+        String type = analyseType();//todo
+        if (peek().getTokenType() == TokenType.ASSIGN) {
+            expect(TokenType.ASSIGN);
+            if (isExpr())
+                analyseExpr();
+            else
+                throw new Error("Expression illegal");
+        }
+        expect(TokenType.SEMICOLON);
+    }
+
+    private void analyseIfStmt() throws CompileError {
+        // if_stmt -> 'if' expr block_stmt ('else' (block_stmt | if_stmt))?
+
+        expect(TokenType.IF_KW);
+        if (isExpr())
+            analyseExpr();
+        else
+            throw new Error("Expression illegal");
+        if (nextIf(TokenType.ELSE_KW) != null) {
+            if (peek().getTokenType() == TokenType.L_BRACE) {
+                analyseBlockStmt();
+            } else if (peek().getTokenType() == TokenType.IF_KW) {
+                analyseIfStmt();
+            } else {
+                throw new Error("If Statement not completed");
+            }
+        }
+    }
+
+    private void analyseWhileStmt() throws CompileError {
+        // while_stmt -> 'while' expr block_stmt
+
+        expect(TokenType.WHILE_KW);
+        if (isExpr())
+            analyseExpr();
+        else
+            throw new Error("Expression illegal");
+        analyseBlockStmt();
+    }
+
+    private void analyseReturnStmt() throws CompileError {
+        // return_stmt -> 'return' expr? ';'
+
+        expect(TokenType.RETURN_KW);
+        if (isExpr())
+            analyseExpr();
+        expect(TokenType.SEMICOLON);
+    }
+
+    private void analyseExpr() throws CompileError {
+        // expr -> operator_expr | negate_expr | assign_expr | as_expr
+        //       | call_expr | literal_expr | ident_expr | group_expr
+
+        TokenType tt = peek().getTokenType();
+        if (tt == TokenType.MINUS) {
+            //
+        } else if (tt == TokenType.IDENT) {
+            //
+        } else if (tt == TokenType.L_PAREN) {
+            //
+        } else {
+            //
+        }
+
+        throw new Error("Not implemented");
+    }
+
+    private String analyseType() throws CompileError {
+        // ty -> IDENT
+        // IDENT:void/int(/double)
+
+        Token token = expect(TokenType.IDENT);
+        String value = (String) token.getValue();
+        if (value.equals("void") || value.equals("int") || value.equals("double")) {
+            return value;
+        } else {
+            throw new Error("Type illegal");
+        }
+    }
+
+    private boolean isExpr() throws CompileError {
+        TokenType tt = peek().getTokenType();
+        return tt == TokenType.MINUS  || tt == TokenType.IDENT || tt == TokenType.L_PAREN ||
+                tt == TokenType.UINT_LITERAL || tt == TokenType.DOUBLE_LITERAL ||
+                tt == TokenType.STRING_LITERAL || tt == TokenType.CHAR_LITERAL;
+    }
+}
---
+++ src/main/java/miniplc0java/instruction/Instruction.java
@@ -0,0 +1,46 @@
+package miniplc0java.instruction;
+
+import java.util.Objects;
+
+public class Instruction {
+    private Operation opt;
+    Object x;
+
+    public Instruction(Operation opt) {
+        this.opt = opt;
+        this.x = null;
+    }
+
+    public Instruction(Operation opt, Integer x) {
+        this.opt = opt;
+        this.x = x;
+    }
+
+    public Instruction(Operation opt, Long x) {
+        this.opt = opt;
+        this.x = x;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Instruction that = (Instruction) o;
+        return opt == that.opt && Objects.equals(x, that.x);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(opt, x);
+    }
+
+    public Operation getOpt() {
+        return opt;
+    }
+
+    public Object getX() {
+        return x;
+    }
+}
