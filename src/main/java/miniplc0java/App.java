18cd48122c275310dc2fcc2854968f40c5cdc5c4
hhidk <295809364@qq.com>
2020-12-26 15:11:28 +0800

    app v1.0

--- src/main/java/miniplc0java/App.java
+++ src/main/java/miniplc0java/App.java
@@ -80,7 +80,7 @@ public class App {
         o0 o00 = new o0(globalTable, functionTables);
 
         output.print(o00.toString());
-        System.out.println(o00.toString());
+        System.err.println(o00.toString());
 
 //        if (result.getBoolean("tokenize")) {
 //            // tokenize
