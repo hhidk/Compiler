bb6004c138d40e62686ce6d35059c31741ceb52d
hhidk <295809364@qq.com>
2020-12-30 19:55:33 +0800

    double vm code

--- src/main/java/miniplc0java/App.java
+++ src/main/java/miniplc0java/App.java
@@ -9,17 +9,11 @@ import java.util.*;
 
 import miniplc0java.analyser.Analyser;
 import miniplc0java.analyser.FunctionTable;
-import miniplc0java.analyser.SymbolEntry;
 import miniplc0java.analyser.SymbolTable;
 import miniplc0java.error.CompileError;
-import miniplc0java.instruction.Instruction;
 import miniplc0java.tokenizer.StringIter;
-import miniplc0java.tokenizer.Token;
 
-import miniplc0java.tokenizer.TokenType;
 import miniplc0java.tokenizer.Tokenizer;
-import miniplc0java.vm.FunctionDef;
-import miniplc0java.vm.GlobalDef;
 import miniplc0java.vm.o0;
 import net.sourceforge.argparse4j.*;
 import net.sourceforge.argparse4j.impl.Arguments;
@@ -83,38 +77,6 @@ public class App {
         System.out.println(o00.toString());
         output.print(o00.toVmCode());
 
-//        if (result.getBoolean("tokenize")) {
-//            // tokenize
-//            var tokens = new ArrayList<Token>();
-//            try {
-//                while (true) {
-//                    var token = tokenizer.nextToken();
-//                    if (token.getTokenType().equals(TokenType.EOF)) {
-//                        break;
-//                    }
-//                    tokens.add(token);
-//                }
-//            } catch (Exception e) {
-//                // 遇到错误不输出，直接退出
-//                System.err.println(e);
-//                System.exit(0);
-//                return;
-//            }
-//            for (Token token : tokens) {
-//                output.println(token.toString());
-//            }
-//        } else if (result.getBoolean("analyse")) {
-//            // analyze
-//            var analyzer = new Analyser(tokenizer);
-//            Map<String, Object> map = analyzer.analyse();
-//            HashMap<String, SymbolEntry> globalTable = (HashMap<String, SymbolEntry>) map.get("globalTable");
-//            HashMap<String, FunctionTable> functionTables = (HashMap<String, FunctionTable>) map.get("functionTables");
-//            o0 o00 = new o0(globalTable, functionTables);
-//            // printO0(o00, output);
-//        } else {
-//            System.err.println("Please specify either '--analyse' or '--tokenize'.");
-//            System.exit(3);
-//        }
     }
 
     private static ArgumentParser buildArgparse() {
